3be0f3e011b88df0654e2b50633048ad9547ad01
arielszabo <arielszabo@gmail.com>
2018-11-22 23:04:55 +0200

    add caching

--- Model_CV.py
+++ Model_CV.py
@@ -1,9 +1,13 @@
 import pandas as pd
 import numpy as np
-import os, re
+import os
+import re
+import sys
+import logging
+import spacy
+import datetime
 from sklearn.model_selection import RepeatedStratifiedKFold
 from dask_searchcv import GridSearchCV
-from sklearn.feature_extraction import text
 from sklearn.preprocessing import FunctionTransformer, MaxAbsScaler
 from sklearn.linear_model import LogisticRegression, SGDClassifier
 from sklearn.ensemble import RandomForestClassifier
@@ -13,20 +17,52 @@ from sklearn.svm import SVC
 from sklearn.decomposition import TruncatedSVD
 from sklearn.model_selection import train_test_split
 from nltk.stem import SnowballStemmer
-from sklearn.feature_extraction.text import TfidfVectorizer
-import spacy
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer, TfidfTransformer
+from sklearn import metrics
+import xgboost as xgb
+import hashlib
+import glob
 
 
-class CustomVectorizer(TfidfVectorizer):
-    def __init__(self, language='en', *args, **kwargs):
+def logging_setup(output_folder, output_file_name):
+    file_dirname = os.path.dirname(os.path.abspath(__file__))
+    output_path = os.path.join(file_dirname, output_folder)
+    os.makedirs(output_path, exist_ok=True)
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s : %(levelname)s : %(message)s',
+                        filename=os.path.join(output_path, output_file_name))
+
+    ch = logging.StreamHandler(sys.stdout)
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s : %(message)s'))
+    logging.getLogger('').addHandler(ch)
+
+    logger = logging.getLogger(__name__)
+    return logger
+
+
+def load_exported_file(input_data_path):
+    input_mtime = str(os.path.getmtime(input_data_path)).replace('.', '')
+    files_found = glob.glob(os.path.join('code_output', f'*{input_mtime}*'), recursive=True)
+    if len(files_found) > 1:
+        logging.warning(f"There is more than 1 files:\n {files_found}")
+    elif len(files_found) == 1:
+        logging.info(f"Found a good file so there is no need to compute BOW again:\n {files_found[0]}")
+        return pd.read_csv(os.path.join('code_output', files_found[0]))
+    else:
+        return None
+
+
+class CustomVectorizer(CountVectorizer):
+    def __init__(self, language='xx', *args, **kwargs):
         super(CustomVectorizer, self).__init__(*args, **kwargs)
         self.language = language
+        self.nlp = spacy.load(self.language)  # todo: if it's not english ?
 
     # create the analyzer that will be returned by this method
     def custom_analyzer(self, doc):
         # apply the preprocessing and tokenzation steps
-        nlp = spacy.load(self.language)  # todo: if it's not english ?
-        tokens = nlp(doc.lower())
+
+        tokens = self.nlp(doc.lower())
         lemmatized_tokens = [token.lemma_ for token in tokens]
 
         # use sklearn-vectorizer's _word_ngrams built in method
@@ -67,14 +103,19 @@ def clean_df(data):
     # Drop all the strange, ood, snippet, skit data etc ...
     bad = pd.concat([skit_songs, all_snippet, no_lyrics_in_url, no_english_lyrics])
     data.drop(set(bad.index), inplace=True)
+    logging.info('Clean bad input lyrics')
     data['word_count'] = data['lyrics'].apply(lambda lyrics: len(lyrics.split()))
     # Drop high word count
     data.drop(data.query('word_count > 1300').index, inplace=True)
+    logging.info('Drop high word count')
     # Drop low word count
     data.drop(data.query('word_count < 50').index, inplace=True)
+    logging.info('Drop low word count')
     # Drop duplicates
     data.drop_duplicates(subset=['lyrics'], inplace=True)
+    logging.info('Drop duplicate lyrics')
     data['clean_lyrics'] = data['lyrics'].apply(clean_song_lyrics)
+    logging.info('clean lyrics')
 
     slang_word_convertor = {
         "aint": "are not",
@@ -119,42 +160,67 @@ def clean_df(data):
     }
     for key, value in slang_word_convertor.items():
         data['clean_lyrics'] = data['clean_lyrics'].str.replace(f'[\b\s]({key})[\b\s]', value)
+    logging.info('clean slang word convertor')
+
+    # custom clean names
+    custom_names = set(data.artist)
+    custom_names.update(['bad boy', 'queen bee'])
+    for artist_name in custom_names:
+        data['clean_lyrics'].str.replace(f'[\b\s]({artist_name.lower()})[\b\s]', '')
+    logging.info('clean custom names from lyrics')
 
     return data
 
 
 if __name__ == '__main__':
-    clean_data = clean_df(pd.read_excel(r'lyrics2.xlsx'))
+    logging_setup('code_output', f'rap_model_CV_{str(datetime.datetime.now().date())}.log')
+    input_data = r'lyrics2.xlsx'
+    df = pd.read_excel(input_data)
+    clean_data = clean_df(df)
+
+    pre_model = CustomVectorizer(stop_words='english', ngram_range=(1, 2), language='en', binary=True)
 
     model = Pipeline(
-            [
-                ('vec', CustomVectorizer(binary=True, max_df=0.4, stop_words='english', min_df=0.001,
-                                         ngram_range=(1, 2))),
-                # ('vec', StemmedVectorizerAnalyzer(binary=True, max_df=0.4,
-                #                                   min_df=0.001, ngram_range=(1, 2))),
-                # ('clening', FunctionTransformer(clean_song_lyrics, validate=False)),
-                # ('vec', text.TfidfVectorizer(binary=True, stop_words=english_stop_words, max_df=0.4,
-                #                              min_df=0.001, ngram_range=(1, 2))),
-                # ('SVD',  TruncatedSVD(n_components=100, n_iter=7)),
-                ('min_max', MaxAbsScaler()),
-                ('SGD', SGDClassifier(loss='log', penalty='l2', alpha=0.0001, l1_ratio=0.3,
-                                      max_iter=1000, epsilon=0.1, n_jobs=-1,
-                                      learning_rate='optimal', eta0=0.0, power_t=0.0,
-                                      early_stopping=True, validation_fraction=0.15, n_iter_no_change=5))
-                # ('logreg', LogisticRegression(penalty='l1', dual=False, tol=0.0001, C=0.1, max_iter=100))
-                # ('svm', SVC(gamma=3, C=100))
-            ]
-        )
-
-    # custom clean names
-    custom_names = set(clean_data.artist)
-    custom_names.update(['bad boy', 'queen bee'])
-    for artist_name in custom_names:
-        clean_data['clean_lyrics'].str.replace(f'[\b\s]({artist_name.lower()})[\b\s]', '')
+        [
+            ('tfidf', TfidfTransformer()),
+            ('xgb', xgb.XGBClassifier(max_depth=3, learning_rate=0.1, n_estimators=100, silent=True,
+                                      objective='binary:logistic', booster='gblinear', n_jobs=-1, nthread=None,
+                                      gamma=0, min_child_weight=1, max_delta_step=0, subsample=1,
+                                      colsample_bytree=1,
+                                      colsample_bylevel=1, reg_alpha=0, reg_lambda=1))
+            # ('svm', SVC(gamma=3, C=100))
+        ]
+    )
 
     X = clean_data['clean_lyrics']
     y = clean_data['rapper_type']
 
+    logging.info('Starting_to_train')
+    x_train, x_test, y_train, y_test = train_test_split(X, y, stratify=y)
+
+    # The next chapter is for run time saving, there is no need to do all the CustomVectorizer
+    # if we already did it once and the data haven't changed:
+
+    vec_train = load_exported_file(input_data)
+    # There isn't a file with the same name as the last modified date of the input data (aka data has changed):
+    if not vec_train:
+        vec_train_arr = pre_model.fit_transform(x_train)
+        np.save(vec_train_arr, f'vec_train_{str(os.path.getmtime(input_data)).replace(".", "")}.npy')
+        # vec_feature_names = pre_model.get_feature_names()
+        # vec_train = pd.DataFrame(vec_train_arr.toarray(), columns=pre_model.get_feature_names())
+        # vec_train.to_csv(f'vec_train_{str(os.path.getmtime(input_data)).replace(".", "")}.csv', chunksize=100)
+
+    # Fit the rest of the model
+
+    model.fit(vec_train, y_train)
+    y_pred_train = model.predict(vec_train)
+    logging.info(metrics.classification_report(y_train, y_pred_train))
+
+    x_test_vec = pre_model.transform(x_test)
+    y_pred = model.predict(x_test_vec)
+    logging.info(metrics.classification_report(y_test, y_pred))
+
+    exit()
     print(MLTester(model, X, y, scoring_method='f1_weighted', n_jobs=3, splitting_method=RepeatedStratifiedKFold,
                    splitting_method_params={'n_splits': 5}).run())
 
